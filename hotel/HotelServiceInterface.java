ebd15773901ae3b0039a08f060ec29cf7314dcef
Rucha Patil <116936334+ruchapatil2004@users.noreply.github.com>
2025-05-09 22:15:03 +0530

    Add files via upload

---
+++ hotel/HotelServiceInterface.java
@@ -0,0 +1,7 @@
+package Hotel;
+import java.rmi.Remote; 
+import java.rmi.RemoteException; 
+public interface HotelServiceInterface extends Remote { 
+ boolean bookRoom(String guestName, int roomNumber) throws RemoteException; 
+ boolean cancelBooking(String guestName) throws RemoteException; 
+}
